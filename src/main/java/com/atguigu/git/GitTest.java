7ff2e47c2fb2ac46032dd909f8e9359da096cfb7
Hdhm1996 <89644295+Hdhm1996@users.noreply.github.com>
2021-08-29 23:13:53 +0800

    pull

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello 3");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("pull");
     }
 }
