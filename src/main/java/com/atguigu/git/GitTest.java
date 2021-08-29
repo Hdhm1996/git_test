7170361a655187bb3e81a1897b03d57e16dfc5c1
duanhm <duanhm@163.com>
2021-08-29 22:36:03 +0800

    hot-fix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello 1");
         System.out.println("hello 2");
+        System.out.println("hello 3");
     }
 }
